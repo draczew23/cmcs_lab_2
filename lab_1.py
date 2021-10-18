fa77ae4bf99bded578dbf991c1be4e01c36e3b45
draczew23 <kuba.wiechnik23@gmail.com>
2021-10-18 16:52:03 +0200

    something is wrong with switching pins

--- lab_1.py
+++ lab_1.py
@@ -1,9 +1,11 @@
 import matplotlib.pyplot as plt
 import matplotlib as mpl
 import numpy as np
-print("hey")
+from numpy.lib.index_tricks import diag_indices
 
-zero = np.matrix([
+# spins from -1 to 1
+
+zero = np.array([
 0,1,1,1,0,
 1,0,0,0,1,
 1,0,0,0,1,
@@ -12,7 +14,9 @@ zero = np.matrix([
 0,1,1,1,0
 ])
 
-one=np.matrix([
+zero = np.where(zero != 0, zero, -1)
+
+one=np.array([
 0,1,1,0,0,
 0,0,1,0,0,
 0,0,1,0,0,
@@ -21,7 +25,9 @@ one=np.matrix([
 0,0,1,0,0
 ])
 
-two=np.matrix([
+one = np.where(one != 0, one, -1)
+
+two=np.array([
 1,1,1,0,0,
 0,0,0,1,0,
 0,0,0,1,0,
@@ -30,6 +36,70 @@ two=np.matrix([
 1,1,1,1,1,
 ])
 
+two = np.where(two != 0, two, -1)
 
-test_ar = np.random.randint(2, size=(5, 6))
-print(test_ar)
+noisy0=np.array([
+0,1,1,1,0,
+1,0,0,0,0,
+1,0,0,0,1,
+0,0,0,0,0,
+0,0,0,0,0,
+0,0,1,0,0,
+])
+
+noisy0 = np.where(noisy0 != 0, noisy0, -1)
+
+noisy2=np.array([
+1,1,1,0,0,
+0,0,0,0,0,
+0,0,0,0,0,
+0,0,1,0,0,
+1,0,0,0,0,
+1,1,0,0,1,
+])
+
+noisy2 = np.where(noisy2 != 0, noisy2, -1)
+
+noisy2b=np.array([
+1,1,1,0,0,
+0,0,0,1,0,
+0,0,0,1,0,
+0,0,0,0,0,
+0,0,0,0,0,
+0,0,0,0,0,
+])
+
+noisy2b = np.where(noisy2b != 0, noisy2b, -1)
+
+time_step = 1
+noisy0_copy = noisy0
+
+tens0 = np.tensordot(zero, zero, axes = 0)
+tens1 = np.tensordot(one, one, axes = 0)
+tens2 = np.tensordot(two, two, axes = 0)
+tens_sum = tens0 + tens1 + tens2
+
+one_ar = np.ones((30,30))
+diag_matrix = np.diag(np.diag(one_ar))
+
+w_matrix = 1/3 * (tens0 + tens1 + tens2) - diag_matrix
+
+energy = np.zeros(diag_matrix.shape)
+
+for t in range(time_step):
+    a = np.sum(np.dot(w_matrix, noisy0_copy)) * noisy0_copy
+    energy += -1/2 * a 
+    #energy = np.where(a < 0, energy, 1)
+    a_where_lower = np.where(a < 0)
+    noisy0_copy[a_where_lower] = 1
+
+print(a)
+#print(noisy0)
+print("_________________________")
+#print(noisy0_copy)
+#print(energy)
+#
+
+
+
+
