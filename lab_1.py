6243a5fdf55c5fb664dea81d594d94944e02f21e
draczew23 <kuba.wiechnik23@gmail.com>
2021-10-18 12:49:11 +0200

    graph test

--- lab_1.py
+++ lab_1.py
@@ -1,5 +1,7 @@
-print("Hello world!")
-
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 import numpy as np
 
-ar = np.array
+x = np.linspace(0, 20, 100)
+plt.plot(x, np.sin(x))
+plt.show()
